c25316e393ecc3aedaab4a414712451c1d5f1bcc
dominx99 <dominx992@gmail.com>
2023-10-28 19:23:52 +0200

    Add ItemEntity to the package

--- plugins/inventory/server/src/interfaces/ItemEntity.ts
+++ plugins/inventory/server/src/interfaces/ItemEntity.ts
@@ -1,3 +1,15 @@
-export interface ItemEntity {
-    id: string
-}
+import { ItemModel as RPGItemModel } from "@rpgjs/server/lib/models/Item";
+import { ItemType } from "../enum/ItemType";
+import { EquippedGraphics } from "./EquippedGraphics";
+
+type ItemEntity = RPGItemModel & {
+    equippable: boolean,
+    displayName: string,
+    type: ItemType,
+    icon: string,
+    graphic?: EquippedGraphics;
+    atk?: number;
+    pdef?: number;
+};
+
+export { ItemEntity };
